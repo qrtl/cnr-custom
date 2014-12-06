6f296cb4ec9f0d600e08d9168f4ce85ff7d4fe1c
Yoshi Tashiro <tashiro@roomsfor.hk>
2014-12-06 16:17:36 +0800

    update product_enhanced

--- product_enhanced/product.py
+++ product_enhanced/product.py
@@ -20,12 +20,14 @@
 ##############################################################################
 
 from openerp.osv import fields, osv
+from openerp.tools.translate import _
 
 class product_template(osv.osv):
     _inherit = "product.template"
     _columns = {
         'name_furigana': fields.char('Furigana', select=True),
-        'packaging': fields.selection([('barrel','Barrel'),('circular','Circular Box'),('cup','Cup'),('box','Box'),('pack','Pack')], 'Packaging'),
+#        'packaging': fields.selection([('barrel','Barrel'),('circular','Circular Box'),('cup','Cup'),('box','Box'),('pack','Pack')], 'Packaging'),
+        'packaging': fields.many2one('product.packaging.unit', 'Packaging'),
         'shipping_method': fields.selection([('regular','Regular'),('cool','Cool'),('frozen','Frozen')], 'Shipping Method'),
         'display_stock': fields.integer('Display Stock'),
         'ship_height': fields.float('Shipping Height'),
@@ -33,10 +35,40 @@ class product_template(osv.osv):
         'ship_length': fields.float('Shipping Length'),
         'ingredients': fields.text('Materials/Ingredients', translate=True),
         'salt_percent': fields.float('Salt %'),
-        'allergy': fields.selection([('wheat','Wheat'),('egg','Egg'),('peanut','Peanut')], 'Allergy'),
-        'designer': fields.selection([('designer1','Designer 1'),('designer2','Designer 2')], 'Designer'),
+        'allergy_ids': fields.many2many('product.allergy', id1='product_id', id2='allergy_id', string='Allergy'),
+        'designer_ids': fields.many2many('product.designer', id1='product_id', id2='designer_id', string='Designer'),
         'manufacture_country': fields.many2one('res.country', 'Manufacture Country', ondelete='restrict'),
         'alcohol_percent': fields.float('Alcohol %'),
         'buyer': fields.many2one('res.partner', 'Buyer', domain=[('is_company','=',False)]),
         'buyer_commission': fields.float('Buyer Commission'),
     }
+
+class product_packaging_unit(osv.osv):
+    _name = 'product.packaging.unit'
+    _description = 'Product Packaging Unit'
+    _columns = {
+        'name': fields.char('Name', required=True, translate=True),
+    }
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)','The name must be unique.')]
+
+class product_allergy(osv.osv):
+    _name = 'product.allergy'
+    _description = 'Allergy'
+    _columns = {
+        'name': fields.char('Name', required=True, translate=True),
+        'product_ids': fields.many2many('product.template', id1='allergy_id', id2='product_id', string='Products'),
+    }
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)','The name must be unique.')]
+
+class product_designer(osv.osv):
+    _name = 'product.designer'
+    _description = 'Designer'
+    _columns = {
+        'name': fields.char('Name', required=True),
+        'designer_ids': fields.many2many('product.template', id1='designer_id', id2='product_id', string='Designers'),
+    }
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)','The name must be unique.')]
+
